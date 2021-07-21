0fa6dfa3d728b0321fc5597d0963fe42e8d3d35a
Paulo Mouat <paulo.mouat@gmail.com>
2021-07-21 17:48:01 -0400

    Initial implementation of renaming all references to audio files.

--- rename-audio-files.py
+++ rename-audio-files.py
@@ -4,29 +4,73 @@ import sys, getopt
 import zipfile
 import shutil
 import glob
-from pathlib import Path
+import xml.etree.ElementTree as ET
+from pathlib import Path, PurePath
+from io import BytesIO
 
-working_folder = "_s1rename_temp"
+working_folder_name = "_s1rename_temp"
+xmlns_attribute = 'xmlns:x="http://www.presonus.com/"'
 
 def usage():
     print('rename-audio-files.py -s <song-file> -f <media-folder> -i <input-prefix> -o <output-prefix>')
     sys.exit(2)
 
+def prepare_xml_file(file, root_tag):
+    lines = []
+    with open(file, encoding='UTF-8') as f:
+        lines = f.readlines()
+    
+    root_str = '<' + root_tag
+    processed = []
+    for idx, line in enumerate(lines):
+        if line.startswith(root_str):
+            line = line.replace(root_tag, root_tag + ' ' + xmlns_attribute, 1)
+        processed.append(line)
+
+    xml = ''.join(processed)
+    return xml
+
+def restore_xml(xml, root_tag):
+    lines = xml.splitlines()
+    
+    root_str = '<' + root_tag
+    processed = []
+    for idx, line in enumerate(lines):
+        if line.startswith(root_str):
+            line = line.replace(root_tag + ' ' + xmlns_attribute, root_tag, 1)
+        processed.append(line)
+
+    restored = '\n'.join(processed)
+    return restored
+
+def get_song_file(songfile):
+    full = PurePath(songfile)
+    path = full.parent
+    stem = full.stem
+    suffix = full.suffix
+    return {'full': full, 'path': path, 'stem': stem, 'suffix': suffix}
+
+def get_working_folder(songfile):
+    path = Path(songfile['path']) / working_folder_name
+    return path
+
 def prepare_song(songfile):
-    shutil.copyfile(songfile, songfile + ".rename-bak")
+    shutil.copyfile(songfile['full'], str(songfile['full']) + ".rename-bak")
 
-    dirpath = Path(working_folder)
-    if dirpath.exists() and dirpath.is_dir():
-        shutil.rmtree(dirpath)
+    working_folder = get_working_folder(songfile)
+    if working_folder.exists() and working_folder.is_dir():
+        shutil.rmtree(working_folder)
 
-    with zipfile.ZipFile(songfile, 'r') as zip_ref:
+    with zipfile.ZipFile(songfile['full'], 'r') as zip_ref:
         zip_ref.extractall(working_folder)
 
 def finalize_song(songfile):
-    shutil.make_archive(songfile, 'zip', working_folder)
-    shutil.move(songfile + '.zip', songfile)
+    working_folder = get_working_folder(songfile)
+    path_and_stem = songfile['path'] / songfile['stem']
+    shutil.make_archive(path_and_stem, 'zip', working_folder)
+    shutil.move(str(path_and_stem) + '.zip', str(path_and_stem) + songfile['suffix'])
 
-def get_files_to_rename(mediafolder, inputprefix, outputprefix):
+def get_files_to_rename(songfile, mediafolder, inputprefix, outputprefix):
     inputpath = mediafolder + "/" + inputprefix
     outputpath = mediafolder + "/" + outputprefix
     
@@ -45,8 +89,13 @@ def get_files_to_rename(mediafolder, inputprefix, outputprefix):
     len_existing = len(existing)
     zfill_count = 2 if len_existing < 100 else len(str(len_existing))
     for idx, original in enumerate(existing):
-        renamed = outputpath + '-' + str(idx+1).zfill(zfill_count) + '.wav'
-        rename_list.append({'original': original, 'renamed': renamed})
+        originalFile = PurePath(original)
+        originalPath = originalFile.parent
+        originalStem = originalFile.stem
+        originalSuffix = originalFile.suffix
+        renamedStem = outputprefix + '-' + str(idx+1).zfill(zfill_count)
+        renamedFile = originalPath / (renamedStem + originalSuffix)
+        rename_list.append({'originalFile': originalFile, 'originalStem': originalStem, 'renamedFile': renamedFile, 'renamedStem': renamedStem})
 
     return rename_list
 
@@ -56,10 +105,47 @@ def get_events_to_rename(files_to_rename):
 
 def rename_files(files_to_rename):
     for idx, rename in enumerate(files_to_rename):
-        shutil.copy(rename['original'], rename['renamed'])
+        shutil.copy(rename['originalFile'], rename['renamedFile'])
     return files_to_rename
 
-def rename_file_references(files_to_rename):
+def rename_file_references(songfile, files_to_rename):
+    # files are referenced using URL notation
+    replacements = {'file://' + str(e['originalFile']): 'file://' + str(e['renamedFile']) for e in files_to_rename}
+
+    # file references are all contained in the file Song/mediapool.xml
+    working_folder = get_working_folder(songfile)
+    mediapool = PurePath(working_folder) / 'Song' / 'mediapool.xml'
+
+    shutil.copyfile(mediapool, str(mediapool) + ".rename-bak")
+
+    # Studio One does not include xml namespace declarations in its files
+    # and ElementTree can't handle elements with undeclared xml namespaces,
+    # so we need to preprocess the file and add a bogus declaration
+    xml = prepare_xml_file(mediapool, "MediaPool")
+    root = ET.fromstring(xml)
+    for audioClip in root.iter('AudioClip'):
+        urlElements = audioClip.findall("Url[@{http://www.presonus.com/}id='path']")
+        for urlElement in urlElements:
+            url = urlElement.attrib['url']
+            if url in replacements:
+                urlElement.attrib['url'] = replacements[url]
+
+    et = ET.ElementTree(root)
+    # need to register the namespace with prefix 'x' so that the
+    # output matches what Studio One produces
+    ET.register_namespace("x", "http://www.presonus.com/")
+
+    # fake file to obtain a string representation of the whole
+    # file so that we can post-process it to remove the bogus
+    # xml namespace declaration from the root element
+    fake_file = BytesIO()
+    et.write(fake_file, encoding='utf-8', xml_declaration=True) 
+    xml_with_changes = fake_file.getvalue().decode('utf-8')
+    processed = restore_xml(xml_with_changes, "MediaPool")
+
+    with open(str(mediapool) + ".new.xml", "w") as mediapool_file:
+        mediapool_file.write(processed)
+
     return files_to_rename
 
 def rename_event_references(events_to_rename):
@@ -68,7 +154,7 @@ def rename_event_references(events_to_rename):
 def main(argv):
     inputprefix = ''
     outputprefix = ''
-    songfile = ''
+    songfileOpt = ''
     mediafolder = ''
 
     try:
@@ -82,20 +168,23 @@ def main(argv):
         elif opt in ("-o", "--outputprefix"):
             outputprefix = arg
         elif opt in ("-s", "--songfile"):
-            songfile = arg
+            songfileOpt = arg
         elif opt in ("-f", "--mediafolder"):
             mediafolder = arg
 
-    if inputprefix == '' or outputprefix == '' or songfile == '' or mediafolder == '':
+    if inputprefix == '' or outputprefix == '' or songfileOpt == '' or mediafolder == '':
         usage()
 
-    prepare_song(songfile)
-    files_to_rename = get_files_to_rename(mediafolder, inputprefix, outputprefix)
+    songfile = get_song_file(songfileOpt)
+
+    #prepare_song(songfile)
+    files_to_rename = get_files_to_rename(songfile, mediafolder, inputprefix, outputprefix)
     rename_files(files_to_rename)
-    rename_file_references(files_to_rename)
+    rename_file_references(songfile, files_to_rename)
     events_to_rename = get_events_to_rename(files_to_rename)
     rename_event_references(events_to_rename)
-    finalize_song('zipped.song')
+    songfile['stem'] = 'zipped'
+    finalize_song(songfile)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
